f320fda8a15cc1db1bb6a76d2ef1dfd6db34ca1c
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2024-10-06 13:03:10 +0530

    feat: `Todo` Controller created

---
+++ src/main/java/com/codejourney/Todo/controllers/TodoController.java
@@ -0,0 +1,28 @@
+package com.codejourney.Todo.controllers;
+
+import com.codejourney.Todo.models.Todo;
+import com.codejourney.Todo.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> getAll() {
+        return todoService.getList();
+    }
+
+
+}
